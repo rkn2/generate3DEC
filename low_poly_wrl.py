c38eab5ba1373517f4927efdb8fad68d93c082e6
rkn2 <rkn2@princeton.edu>
2017-10-26 14:48:56 -0400

    exports low poly count meshes individually

--- low_poly_wrl.py
+++ low_poly_wrl.py
@@ -1,70 +1,46 @@
-##works, suppresses dialog box
-#
-#import rhinoscriptsyntax as rs
-## iterates through layers
-#layers = rs.LayerNames() 
-#for layer in layers:
-#    
-#    # select layer
-#    rs.Command("-_SelLayer " + layer)
-#    
-#    directory = 'C:\\Users\\Rebecca Napolitano\\Documents\\datafiles\\Romanbondingcourses\\2017_10_25_experiments\\test2\\'
-#    
-#    filename = 'test'
-#    filetype = '.wrl'
-#    
-#    # make cmdstr, include layer if there are multiple layers
-#    if len(layers) > 1:
-#        path = "\"" + directory + filename + "_" + layer + filetype + "\""
-#    else:
-#        path = "\"" + directory + filename + filetype + "\""
-#    cmdstr = "-_Export " + path
-#    if filetype == ".wrl":
-#        cmdstr += " Enter"
-#        cmdstr += " Angle 0.0 AspectRatio 0.0 Distance 0.1 Density 0.1 Grid 0.0 "
-#        cmdstr += " enter " 
-#                
-#        #cmdstr += " Enter Enter"
-#    
-#    # execute command
-#    cmd = rs.Command(cmdstr)
-#    if not(cmd):
-#        success = False
-#        
-#    rs.Command("-_SelNone")
-#    
-#___________________________________________________________________________________________
+#works, suppresses dialog box
 
 import rhinoscriptsyntax as rs
 # iterates through layers
 layers = rs.LayerNames() 
 for layer in layers:
     
-    # select layer
-    rs.Command("-_SelLayer " + layer)
+    if layer != 'concrete':
     
-    directory = 'C:\\Users\\Rebecca Napolitano\\Documents\\datafiles\\Romanbondingcourses\\2017_10_25_experiments\\test2\\'
-    
-    filename = 'test'
-    filetype = '.wrl'
-    
-    # make cmdstr, include layer if there are multiple layers
-    if len(layers) > 1:
-        path = "\"" + directory + filename + "_" + layer + filetype + "\""
-    else:
-        path = "\"" + directory + filename + filetype + "\""
-    cmdstr = "_Export " + path
-    #if filetype == ".wrl":
-        #cmdstr += " Enter"
-        #cmdstr += " Angle 0.0 AspectRatio 0.0 Distance 0.1 Density 0.1 Grid 0.0 "
-        #cmdstr += " enter " 
-                
-        #cmdstr += " Enter Enter"
-    
-    # execute command
-    cmd = rs.Command(cmdstr)
-#    if not(cmd):
-#        success = False
-#        
-    rs.Command("-_SelNone")
+        # select layer
+        rs.Command("-_SelLayer " + layer)
+        
+        rs.Command("-_Mesh DetailedOptions SimplePlane=Yes Enter")
+           
+        directory = 'C:\\Users\\Rebecca Napolitano\\Documents\\datafiles\\Romanbondingcourses\\2017_10_26_experiments\\'
+        
+        filename = 'test'
+        filetype = '.wrl'
+        
+        # make cmdstr, include layer if there are multiple layers
+        if len(layers) > 1:
+            path = "\"" + directory + filename + "_" + layer + filetype + "\""
+        else:
+            path = "\"" + directory + filename + filetype + "\""
+        
+        rs.Command("-_SelNone ")
+        rs.Command("-_SelLayer " + layer)
+        rs.Command("-_Invert ")
+        rs.Command("Hide Enter")
+        rs.Command("-_SelMesh ")
+        cmdstr = "-_Export " + path
+        if filetype == ".wrl":
+            cmdstr += " Enter Enter"
+            #cmdstr += " Angle 0.0 AspectRatio 0.0 Distance 0.1 Density 0.1 Grid 0.0 "
+            #cmdstr += " enter " 
+                    
+            #cmdstr += " Enter Enter"
+        
+        # execute command
+        cmd = rs.Command(cmdstr)
+        if not(cmd):
+            success = False
+            
+        rs.Command("-_SelNone" )
+        rs.Command("Show" )
     
