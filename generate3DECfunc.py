242372670202ae0f73e41d4c566843c78ccd4ba8
rkn2 <rkn2@princeton.edu>
2017-11-10 10:22:33 -0500

    implementing change of jmat2

--- generate3DECfunc.py
+++ generate3DECfunc.py
@@ -161,7 +161,7 @@ class generateFile:
         #setupData = setupData.replace(self.movieInterval, self.movieInterval.strip("'"))
         self.outfile.write(setupData)
         self.outfile.write('\n;---------------------------------RUNTIME-----------------------------------\n')
-        self.outfile.write('\n@setup \n@neighbors \n@initial_centroid \n@initial_vertex \n@getvol \n@getstoneid \n@movieSetup \n@makeMoviePlots \n@cycloop \n@plot_cracks \n@makeCrackPlots \n@plotCrackPlot \n@displacement \n@final_centroid \n@final_vertex \n@get_stress \n@clearPlots')
+        self.outfile.write('\n@setup \n@normals \n@neighbors \n@initial_centroid \n@initial_vertex \n@getvol \n@getstoneid \n@movieSetup \n@makeMoviePlots \n@cycloop \n@plot_cracks \n@makeCrackPlots \n@plotCrackPlot \n@displacement \n@final_centroid \n@final_vertex \n@get_stress \n@clearPlots')
         
 
     def joinFiles(self,i):
