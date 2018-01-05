bc8c1b73af5d97ff1f998d7ef87ebd924ec51fa8
rkn2 <rkn2@princeton.edu>
2018-01-05 09:29:55 -0500

    fileHandles grabbed correctly

--- generate_3dec3.0.py
+++ generate_3dec3.0.py
@@ -1,3 +1,5 @@
+import glob
+
 class material():
     # stores all metadata about a material
     def __init__(self, properties):
@@ -30,33 +32,49 @@ class experiment():
             self.geometries.append(newGeo)
         if mat not in self.materials:
             self.materials.append(mat)
+    
+    def getFileHandles(self):
+        fileHandles = {}
+        #gets all the fileHandles
+        for geometry in self.geometries:
+            fileHandles[geometry.label] = glob.glob(self.filePath + '*_' + geometry.label + '.3ddat')
+            #checks to make sure all input geometries have correponding files
+            if fileHandles == []:
+                print('There are no geometry files of type ' + geometry.label)
+        print(fileHandles)      
+        
     def writeGeometry(self):        
         # first assign indices to unique materials
         for i in range(len(self.materials)):
             self.materials[i].idx = i
-        # write base geometry first
         
-        # then loop over other geometries
+        # next we need to get a list of the files so we can call them for writing
+        # so lets make a list called fileHandles, we can do that in a different function above
+        
+        self.getFileHandles()
+        
+        
+
 
 #============================================================
 
 # set up experiment
-filePath= 'C:\\test\\file\\path\\'
+filePath= 'C:\\Users\\Rebecca Napolitano\\Documents\\datafiles\\test\\'
 my_experiment = experiment(filePath)
 
 # define materials
-brick = material({'dens':2000})
-concrete = material({'dens':2200})
+mortar = material({'dens':2200})
 stone = material({'dens':2400})
+fixedstone = material({'dens':2400,'fixity':True}) #could not copy to work
 
-# create a copy of stone but add fixity
-fixedStone = material(dict(stone.properties))
-fixedStone['fixity'] = True
+## create a copy of stone but add fixity
+#fixedStone = material(dict(stone.properties))
+#fixedStone['fixity'] = 'True'
 
 # add geometries to experiment
-my_experiment.addGeometry('base', fixedStone)
-my_experiment.addGeometry('brick', brick)
-my_experiment.addGeometry('concrete', concrete)
+my_experiment.addGeometry('base', fixedstone)
+my_experiment.addGeometry('loadblock', stone)
+my_experiment.addGeometry('mortar', mortar)
 my_experiment.addGeometry('stone', stone)
 
 # write geometries to 3dec file
