e056de28e705302e764c2d4a8e85500b4fba319e
rkn2 <rkn2@princeton.edu>
2018-01-05 10:16:37 -0500

    creates file, writes new line commands, does not have a name yet

--- generate_3dec3.0.py
+++ generate_3dec3.0.py
@@ -6,7 +6,7 @@ class material():
         self.properties = properties
         self.idx = None
     def write(self):
-        # write properties to string
+        # write properties to string !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! LATER I WILL NEED FIXITY, HIDING, AND GEN EDGE TO BE NEW LINES AND APART FROM OTHERS...
         s = ''
         for p in self.properties:
             s += '%s %s'%(p,self.properties[p])
@@ -20,10 +20,16 @@ class geometry():
         
 class experiment():
     # holds all the necessary objects to run an experiment
-    def __init__(self, filePath):
+    def __init__(self, filePath, outFileName, iterator, load_min = None, load_max = None, load_iterator = None):
         self.filePath = filePath
         self.geometries = []
         self.materials = []
+        self.outFileName = outFileName
+        self.iterator = iterator
+        self.load_min = load_min
+        self.load_max = load_max
+        self.load_iterator = load_iterator
+        
     def addGeometry(self, label, mat):
         # create a new geometry object
         newGeo = geometry(label, mat)
@@ -34,14 +40,27 @@ class experiment():
             self.materials.append(mat)
     
     def getFileHandles(self):
-        fileHandles = {}
+        
+        self.fileHandles = {}
         #gets all the fileHandles
         for geometry in self.geometries:
-            fileHandles[geometry.label] = glob.glob(self.filePath + '*_' + geometry.label + '.3ddat')
+            self.fileHandles[geometry.label] = glob.glob(self.filePath + '*_' + geometry.label + '.3ddat')
+            #checks to make sure only base has multiple files !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ERROR HERE.. DOES NOT ACCEPT MULTIPLE BASE FILES
+            length = len(self.fileHandles[geometry.label])
+            if geometry.label != 'base':
+                continue
+            else:
+                if length > 1:
+                    print('There are too many ' + geometry.label + ' files!')
+            #removes filepath and suffix from fileHandles
+            i = 0
+            while i < length:
+                self.fileHandles[geometry.label][i] = self.fileHandles[geometry.label][i].replace('.3ddat', '').replace(self.filePath, '')
+                i = i + 1
             #checks to make sure all input geometries have correponding files
-            if fileHandles == []:
+            if self.fileHandles == []:
                 print('There are no geometry files of type ' + geometry.label)
-        print(fileHandles)      
+                    
         
     def writeGeometry(self):        
         # first assign indices to unique materials
@@ -50,9 +69,31 @@ class experiment():
         
         # next we need to get a list of the files so we can call them for writing
         # so lets make a list called fileHandles, we can do that in a different function above
-        
         self.getFileHandles()
         
+        if iterator == 'base':
+            numSimulations = len(self.fileHandles['base'])
+            for i in range (numSimulations):
+                #get file name    
+                self.fileName = self.fileHandles['base'][i]
+                writeFile = self.fileName = '.3ddat'
+                output = self.filePath + writeFile
+                #open the file and start writing
+                self.outfile = open(output, 'w+')
+                self.outfile.write('\n;-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n')
+                self.outfile.write('new\n' + ';This is file ' + str(i) + '\n')                
+                self.outfile.close()
+            
+            
+            
+            
+#        if iterator == 'load':
+#            numSimulations = load_max/load_iterator
+#            for i in range(numSimulations):
+#                fileName = self.fileHandles['base'][0] + 
+                    # COMEEEEEEEEEEEEEEEEEEEEEEEE BACCCCCCCCCCCCCCCCKKKKKKKKKK LATERRRRRRRRRRRRRRRRRRRRRR
+        
+
         
 
 
@@ -60,12 +101,18 @@ class experiment():
 
 # set up experiment
 filePath= 'C:\\Users\\Rebecca Napolitano\\Documents\\datafiles\\test\\'
-my_experiment = experiment(filePath)
+outFileName = 'TEST_3DEC_INPUT'
+iterator = 'base' #can iterate over base or load !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! IS THIS HARDCODING..?
+#if iterator = 'load' the following parameters need to be specified !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! DEAL WITH THIS AFTER BASE STARTS WORKING
+#load_min = 0
+#load_max = 1000
+#load_iterator = 25 
+my_experiment = experiment(filePath, outFileName, iterator)
 
 # define materials
 mortar = material({'dens':2200})
 stone = material({'dens':2400})
-fixedstone = material({'dens':2400,'fixity':True}) #could not copy to work
+fixedstone = material({'dens':2400,'fixity':'fix'}) #could not copy to work  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! HOW SHOULD I GET FIXITY TO WORK, ITS DIFFERENT THAN OTHER PROPERTIES, NEEDS TO BE OWN LINE AND ONLY SAY "FIX"
 
 ## create a copy of stone but add fixity
 #fixedStone = material(dict(stone.properties))
