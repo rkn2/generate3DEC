a446e775f71d7587e526dba65038cb9266295131
rkn2 <rkn2@princeton.edu>
2018-04-25 14:51:53 -0400

    fixes settle2 also

--- removeUnphysicalSettlements.py
+++ removeUnphysicalSettlements.py
@@ -8,11 +8,15 @@ os.chdir(file_path)
 fileHandles = glob.glob('*.3ddat')
 
 for fileName in fileHandles:
-    if 'settle2' not in fileName:
+    if 'settle3' in fileName:
         file = fileName.replace('.3ddat','').replace('settle3_','') #remove extra characters
         file = np.fromstring(file,sep='_')[:]
         if file[3] == file[4] and file[4] == file[5]:
             os.remove(fileName)
-            
-            
-            #2384 before, 2244 now.. not much of a difference but still something 
+    if 'settle2' in fileName:
+        file = fileName.replace('.3ddat','').replace('settle2_','') #remove extra characters
+        file = np.fromstring(file,sep='_')[:]
+        if file[2] == file[1]:
+            os.remove(fileName)
+    
+    #2384 before, 2208 now.. not much of a difference but still something 
