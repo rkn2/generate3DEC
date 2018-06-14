f059361916b1abe5e2caa9c5568d4ca93dee964b
rkn2 <rkn2@princeton.edu>
2018-06-14 11:02:29 -0400

    new input script for savecyc feature

---
+++ inputScript3.0.py
@@ -0,0 +1,95 @@
+import sys
+import numpy as np
+
+path1 = 'C:\\Users\\Rebecca Napolitano\\Documents\\GitHub\\generate3DEC\\'
+sys.path.insert(0, path1) #navigate to function folder
+
+import generate_3decCurrent as gen
+
+#============================================================
+#INPUT SCRIPT
+# set up experiment
+filePath= 'C:/Users/Rebecca Napolitano/Documents/datafiles/labWalls/diffusionMaps/current/eqonly/'  
+functionPath = 'C:\\Users\\Rebecca Napolitano\\Documents\\GitHub\\generate3DEC\\'
+outFileName = '2018_6_14_additionalEQ__3DEC'
+iterator = ['base','load'] #can iterate over base or load or stone ; always put base first for naming conventions, not load
+cycChoice = 'ratio' #can be ratio or loop or test
+solveRat = 1e-5
+solveRatio = np.log(1e-0/solveRat)/np.log(2)
+saveCycValue = 'no' #can be a yes or no string and turns on if cycstates are saved. good normally, bad for diffusion maps bc memory
+
+#______________________________________________________________
+
+# define functions and movies
+#options = 'getDisplacement', 'getStress', 'getCracks', 'getFinalCentroid', 'getVolume', 'getInitCentroid',
+#           'getInitVert', 'getFinalVert', 'getNeighbors', 'getCrackData'
+# cycle choice is either cycloop or cycratio
+functionHandles = ['getFinalVert']
+
+#options = 'makeMoviePlots', 'makeCrackPlots'
+movieHandles = []
+
+# list movie plots you want 
+#options: 'displacement', 'xdisplacement', 'ydisplacement', 'zdisplacement', 'smaximum', 'sminimum'
+plots = []
+
+#______________________________________________________________
+
+# define materials(dens, edge, fixity, hide, ymod, join) !!!! YOU NEED EDGE AND YOUNGS IF YOU ARE DOING EQ
+#mortar = material({'dens':2200, 'edge':100, 'hide':True})
+fixedstone = gen.material({'edge':0.5, 'dens':2500,'bulk': 2e9, 'shear':1e9, 'fixity':'fix',})
+brick = gen.material({'edge':0.5, 'dens':2508.67, 'bulk': 2e9, 'shear':1e9})
+
+#______________________________________________________________
+
+#define loads
+loadTypes = ['eq'] #can be 'eq','pt', or 'none' right now
+#if you choose 'eq' then you need to provide the following: 
+#eqDStart, eqDIncrement, eqDEnd for the direction the load will be applied. 0 is x and to the left, 90 is y into the page, etc
+eqDStart = 0 #always in radians
+eqDIncrement = np.pi/16
+eqDEnd = np.pi/2
+eqDList = []
+eqDList = list(np.arange(eqDStart, eqDEnd + eqDIncrement*0.5, eqDIncrement))
+#eqVert, eqFw, eqSStart, eqSIncrement, eqSEnd for the calcuation of the load; eqS is scale of the eq ranging generally from 0 to 0.3g
+#eqVertVal = 744
+#eqFwVal = 2183.15
+eqFreqVal = 3.47
+eqSStart = 0.05
+eqSIncrement = 0.01
+eqSEnd = 0.3
+eqSList = []
+eqSList = list(np.arange(eqSStart, eqSEnd + eqSIncrement*0.5, eqSIncrement)) #cant calculate eqBoundLoad outhere since it will change with each eqS
+#if you choose 'pt' then you need to provide the following:
+#ptVStart, ptVIncrement, ptVEnd for the value of the load thatwill be applied
+ptVStart = 0.0
+ptVIncrement = 0.01
+ptVEnd = 0.0
+ptVList = []
+ptVList = list(np.arange(ptVStart, ptVEnd + ptVIncrement*0.5, ptVIncrement))
+#print(ptVList)
+##ptLList = [] is a list of string locations in the format: range x -1000 1000; can be written in another python script but not here bc too much variation
+ptLList = [["none"]]#'y', 'range z 0.33 0.6']]
+
+#______________________________________________________________
+
+# pass input variables 
+#for loadLocation, it goes bound VALUE range YYY; where YYY can be 'group GROUPNAME', 'x XCOORD y YCOORD z ZCOORD'
+#sample of the variables that can be included see readme for more information
+my_experiment = gen.experiment(filePath, functionPath, outFileName, iterator, cycChoice, functionHandles, movieHandles, plots, solveRatio, loadTypes,
+                               eqFreq = eqFreqVal, eqD = eqDList, eqS = eqSList, 
+                               ptV = ptVList, ptL = ptLList, saveCyc = saveCycValue,
+                               numCycLoops = 1, numCycles = 100, arraySize = 3000000)
+
+# define joint materials
+mortar_stone = gen.jointMaterial({'jkn':8e7, 'jks':5e7, 'jfric': 30})
+
+#______________________________________________________________
+
+# add geometries to experiment
+my_experiment.addGeometry('base', fixedstone, mortar_stone)
+my_experiment.addGeometry('brick', brick, mortar_stone)
+#my_experiment.addGeometry('mortar', mortar, mortar_stone)
+
+# write geometries to 3dec file
+my_experiment.write3DECFile()
